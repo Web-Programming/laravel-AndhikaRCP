5523b4330af4b94037a5153213a8fbccc756c89c
AndhikaRCP <andhikarizky2603@gmail.com>
2023-11-13 11:59:16 +0700

    fix form validate

--- laravel-AndhikaRizkyCahyaPutra/resources/views/prodi/create.blade.php
+++ laravel-AndhikaRizkyCahyaPutra/resources/views/prodi/create.blade.php
@@ -5,13 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Form Prodi</title>
-    <link rel="stylesheet" href="{{asset('css/bootrap.min.css')}}">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
         <div class="row pt-4">
             <div class="col">
                 <h2>Form Prodi</h2>
+                @if (session()->has('info'))
+                <div class="alert alert-success">
+                    {{session()->get('info')}}
+                </div>
+                @endif
                 <form action="{{url('prodi/store')}}" method="POST">
                     @csrf
                     <div class="form-group">
